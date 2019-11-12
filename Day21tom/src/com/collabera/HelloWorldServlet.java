a91d5bc18d863fb481231aafaa00632e13a510ae
thomaswtx <thomas.whitetx@outlook.com>
2019-11-12 08:47:54 -0600

    Basic TomCat introduction

---
+++ Day21tom/src/com/collabera/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package com.collabera;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+
+public class HelloWorldServlet extends HttpServlet{
+	
+	public void service(ServletRequest req, ServletResponse res) 
+	throws IOException, ServletException {
+		
+		res.setContentType("text/html");
+		PrintWriter out= res.getWriter();
+		out.print("<html><body>");
+		out.print("<b>hello generic servlet</b>");
+		out.print("</html></body>");
+	}
+}
