b687a3ac38de8a29146035bc73eb0c960b5188df
thomaswtx <thomas.whitetx@outlook.com>
2019-11-14 15:21:19 -0600

    JSON examples

---
+++ Day24JerseyD/src/main/java/com/collabera/rest/MyResource.java
@@ -0,0 +1,26 @@
+package com.collabera.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.collabera.model.Employee;
+
+@Path("myresource")
+public class MyResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getIt() {
+		return "Got it!";
+	}
+
+	@GET
+	@Path("employee")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employee getEmployee() {
+		Employee emp = new Employee("QW", "Jon Snow");
+		return emp;
+	}
+}
